4057968e9df592f90e8ffa0ad0dd50cd53cb03c7
flaviozorzetto <flaviozorzetto11@gmail.com>
2022-05-11 13:12:02 -0300

    working translator

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import ContentWrapper from '../components/ContentWrapper';
 import parse from 'html-react-parser';
@@ -56,97 +54,25 @@ const WrittingText = styled.p`
    font-family: 'Source Code Pro', monospace;
 `;
 
-const timeout = ms => {
-   return new Promise((res, rej) => {
-      setTimeout(() => {
-         res();
-      }, ms);
-   });
-};
-
-async function buildQueue(queueArr, setFinished) {
-   for (let i = 0; i < queueArr.length; i++) {
-      await buildText(queueArr[i][0], queueArr[i][1]);
-   }
-   await timeout(400);
-   setFinished(true);
-}
-
-async function buildText(text, setText) {
-   let fragText = text.split('');
-   let totalLength = fragText.length;
-   let i = 0;
-   let buildingText = [];
-
-   while (buildingText.length != totalLength) {
-      let text = await recursiveText(fragText, buildingText, i);
-      let additional = buildingText.length == totalLength ? '' : '|';
-      setText(text + additional);
-      i++;
-   }
-}
-
-function recursiveText(fullTextArr, currentArr, i) {
-   return new Promise((res, rej) => {
-      currentArr.push(fullTextArr[i]);
-      setTimeout(() => {
-         res(currentArr.join(''));
-      }, 25);
-   });
-}
-
 export default props => {
-   const { t } = useTranslation();
-   const [text_1, setText_1] = useState('');
-   const [text_2, setText_2] = useState('');
-   const [text_3, setText_3] = useState('');
-   const [finished, setFinished] = useState(false);
-
-   const reset = () => {
-      setFinished(false);
-      setText_1('');
-      setText_2('');
-      setText_3('');
-   };
-
-   useEffect(() => {
-      let rendering = setTimeout(() => {
-         reset();
-         buildQueue(
-            [
-               [`<p> ${t('Hello')} </p>`, setText_1],
-               [
-                  `<h1> ${t('preName')} Flavio Esrenko Zorzetto </h1>`,
-                  setText_2,
-               ],
-               [`<p> ${t('indexText')} </p>`, setText_3],
-            ],
-            setFinished
-         );
-      }, 500);
-      return () => {
-         clearTimeout(rendering);
-      };
-   }, [t]);
-
    return (
       <ContentWrapper>
          <Container>
-            <TextWrapper finishedWriting={finished}>
-               {finished ? (
-                  parse(text_1)
+            <TextWrapper finishedWriting={props.ptFinished}>
+               {props.ptFinished ? (
+                  parse(props['pt_text_1'])
                ) : (
-                  <WrittingText>{text_1}</WrittingText>
+                  <WrittingText>{props['pt_text_1']}</WrittingText>
                )}
-               {finished ? (
-                  parse(text_2)
+               {props.ptFinished ? (
+                  parse(props['pt_text_2'])
                ) : (
-                  <WrittingText>{text_2}</WrittingText>
+                  <WrittingText>{props['pt_text_2']}</WrittingText>
                )}
-               {finished ? (
-                  parse(text_3)
+               {props.ptFinished ? (
+                  parse(props['pt_text_3'])
                ) : (
-                  <WrittingText>{text_3}</WrittingText>
+                  <WrittingText>{props['pt_text_3']}</WrittingText>
                )}
             </TextWrapper>
          </Container>
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -3,6 +3,8 @@ import About from '../routes/About';
 import Index from '../routes/Index';
 import SideBar from '../components/SideBar';
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Wrapper = styled.div`
    display: flex;
@@ -13,14 +15,126 @@ const Container = styled.div`
    width: 100%;
 `;
 
+const timeout = ms => {
+   return new Promise((res, rej) => {
+      setTimeout(() => {
+         res();
+      }, ms);
+   });
+};
+
+async function buildQueue(queueArr, setFinished) {
+   for (let i = 0; i < queueArr.length; i++) {
+      await buildText(queueArr[i][0], queueArr[i][1]);
+   }
+   await timeout(400);
+   setFinished(true);
+}
+
+async function buildText(text, setText) {
+   let fragText = text.split('');
+   let totalLength = fragText.length;
+   let i = 0;
+   let buildingText = [];
+
+   while (buildingText.length != totalLength) {
+      let text = await recursiveText(fragText, buildingText, i);
+      let additional = buildingText.length == totalLength ? '' : '|';
+      setText(text + additional);
+      i++;
+   }
+}
+
+function recursiveText(fullTextArr, currentArr, i) {
+   return new Promise((res, rej) => {
+      currentArr.push(fullTextArr[i]);
+      setTimeout(() => {
+         res(currentArr.join(''));
+      }, 25);
+   });
+}
+
 export default () => {
+   const { t, i18n } = useTranslation();
+   const [pt_text_1, pt_setText_1] = useState('');
+   const [pt_text_2, pt_setText_2] = useState('');
+   const [pt_text_3, pt_setText_3] = useState('');
+   const [ptFinished, ptSetFinished] = useState(false);
+
+   const [en_text_1, en_setText_1] = useState('');
+   const [en_text_2, en_setText_2] = useState('');
+   const [en_text_3, en_setText_3] = useState('');
+   const [enFinished, enSetFinished] = useState(false);
+
+   useEffect(() => {
+      let rendering = setTimeout(() => {
+         buildQueue(
+            [
+               [`<p> ${t('Hello', { lng: 'pt' })} </p>`, pt_setText_1],
+               [
+                  `<h1> ${t('preName', {
+                     lng: 'pt',
+                  })} Flavio Esrenko Zorzetto </h1>`,
+                  pt_setText_2,
+               ],
+               [`<p> ${t('indexText', { lng: 'pt' })} </p>`, pt_setText_3],
+            ],
+            ptSetFinished
+         );
+         buildQueue(
+            [
+               [`<p> ${t('Hello', { lng: 'en' })} </p>`, en_setText_1],
+               [
+                  `<h1> ${t('preName', {
+                     lng: 'en',
+                  })} Flavio Esrenko Zorzetto </h1>`,
+                  en_setText_2,
+               ],
+               [`<p> ${t('indexText', { lng: 'en' })} </p>`, en_setText_3],
+            ],
+            enSetFinished
+         );
+      }, 500);
+      return () => {
+         clearTimeout(rendering);
+      };
+   }, []);
+
+   useEffect(() => {
+      console.log(i18n.language);
+
+      // if (ptFinished) {
+      //    pt_setText_1(`<p> ${t('Hello', { lng: 'pt' })} </p>`);
+      //    pt_setText_2(
+      //       `<h1> ${t('preName', { lng: 'pt' })} Flavio Esrenko Zorzetto </h1>`
+      //    );
+      //    pt_setText_3(`<p> ${t('indexText', { lng: 'pt' })} </p>`);
+      // } else if (enFinished) {
+      //    en_setText_1(`<p> ${t('Hello', { lng: 'en' })} </p>`);
+      //    en_setText_2(
+      //       `<h1> ${t('preName', { lng: 'en' })} Flavio Esrenko Zorzetto </h1>`
+      //    );
+      //    en_setText_3(`<p> ${t('indexText', { lng: 'en' })} </p>`);
+      // }
+   }, [t]);
+
    return (
       <Wrapper>
          <SideBar />
          <Container>
             <Routes>
+               <Route
+                  path="/"
+                  element={
+                     <Index
+                        pt_text_1={pt_text_1}
+                        pt_text_2={pt_text_2}
+                        pt_text_3={pt_text_3}
+                        ptFinished={ptFinished}
+                     />
+                  }
+               ></Route>
                <Route path="/about" element={<About />}></Route>
-               <Route path="/" element={<Index />}></Route>
             </Routes>
          </Container>
       </Wrapper>
