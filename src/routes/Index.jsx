d48f22421200137be7902e7c6c81839d48297658
flaviozorzetto <flaviozorzetto11@gmail.com>
2022-05-11 01:58:28 -0300

    added code writing

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import ContentWrapper from '../components/ContentWrapper';
+import parse from 'html-react-parser';
 
 const Container = styled.div`
    display: flex;
@@ -12,6 +13,7 @@ const Container = styled.div`
 
 const TextWrapper = styled.div`
    color: white;
+   position: relative;
 
    p {
       font-size: 1.5rem;
@@ -24,15 +26,50 @@ const TextWrapper = styled.div`
    }
 
    width: 45%;
+
+   ${props => {
+      return props.finishedWriting
+         ? `animation: fadeInFromNone 1s ease-out;`
+         : '';
+   }}
+
+   @keyframes fadeInFromNone {
+      0% {
+         display: none;
+         opacity: 0;
+      }
+
+      1% {
+         display: block;
+         opacity: 0;
+      }
+
+      100% {
+         display: block;
+         opacity: 1;
+      }
+   }
 `;
 
-async function buildQueue(queueArr) {
+const WrittingText = styled.p`
+   font-size: 1rem;
+   font-family: 'Source Code Pro', monospace;
+`;
+
+const timeout = ms => {
+   return new Promise((res, rej) => {
+      setTimeout(() => {
+         res();
+      }, ms);
+   });
+};
+
+async function buildQueue(queueArr, setFinished) {
    for (let i = 0; i < queueArr.length; i++) {
       await buildText(queueArr[i][0], queueArr[i][1]);
    }
-   queueArr.forEach(e => {
-      e[1](<p>{e[0]}</p>);
-   });
+   await timeout(400);
+   setFinished(true);
 }
 
 async function buildText(text, setText) {
@@ -54,7 +91,7 @@ function recursiveText(fullTextArr, currentArr, i) {
       currentArr.push(fullTextArr[i]);
       setTimeout(() => {
          res(currentArr.join(''));
-      }, 10);
+      }, 25);
    });
 }
 
@@ -63,14 +100,29 @@ export default props => {
    const [text_1, setText_1] = useState('');
    const [text_2, setText_2] = useState('');
    const [text_3, setText_3] = useState('');
+   const [finished, setFinished] = useState(false);
+
+   const reset = () => {
+      setFinished(false);
+      setText_1('');
+      setText_2('');
+      setText_3('');
+   };
 
    useEffect(() => {
       let rendering = setTimeout(() => {
-         buildQueue([
-            [`<p> ${t('Hello')} </p>`, setText_1],
-            [`<h1> ${t('preName')} Flavio Esrenko Zorzetto </h1>`, setText_2],
-            [`<p> ${t('indexText')} </p>`, setText_3],
-         ]);
+         reset();
+         buildQueue(
+            [
+               [`<p> ${t('Hello')} </p>`, setText_1],
+               [
+                  `<h1> ${t('preName')} Flavio Esrenko Zorzetto </h1>`,
+                  setText_2,
+               ],
+               [`<p> ${t('indexText')} </p>`, setText_3],
+            ],
+            setFinished
+         );
       }, 500);
       return () => {
          clearTimeout(rendering);
@@ -80,10 +132,22 @@ export default props => {
    return (
       <ContentWrapper>
          <Container>
-            <TextWrapper>
-               {text_1}
-               {text_2}
-               {text_3}
+            <TextWrapper finishedWriting={finished}>
+               {finished ? (
+                  parse(text_1)
+               ) : (
+                  <WrittingText>{text_1}</WrittingText>
+               )}
+               {finished ? (
+                  parse(text_2)
+               ) : (
+                  <WrittingText>{text_2}</WrittingText>
+               )}
+               {finished ? (
+                  parse(text_3)
+               ) : (
+                  <WrittingText>{text_3}</WrittingText>
+               )}
             </TextWrapper>
          </Container>
       </ContentWrapper>
